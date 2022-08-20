12c570e531a1c49f56d18df861c242c875d4ee5a
Maxxjx <maxxssj338@example.com>
2022-08-20 21:21:46 +0530

    ggg

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function logout(){
   document.getElementById("full-card").style.display="none";
   document.getElementById("body").style.overflow="auto";
 }
+// navbar-background-change
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 630 || document.documentElement.scrollTop > 630) {
+    document.getElementById("nav-bar").style.backgroundColor="black";
+    console.log("ooooooooooooooooooo");
+  } else {
+    document.getElementById("nav-bar").style.backgroundColor="transparent";
+    console.log("ooppppppppppppppppppppp");
+  }
+} 
