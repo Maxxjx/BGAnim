ba6510ace96ef9708565aac474f3632be236a7b2
Maxxjx <maxxssj338@example.com>
2022-08-20 12:26:09 +0530

    Added favicon

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function log(){
   document.getElementById("full-card").style.display="block";
+  document.getElementById("body").style.overflow="hidden";
 }
 function logout(){
   document.getElementById("full-card").style.display="none";
+  document.getElementById("body").style.overflow="auto";
 }
