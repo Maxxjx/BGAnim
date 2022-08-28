a435bde617802be75fc79b6d5839f7c7bb1b80ab
Maxxjx <maxxssj338@example.com>
2022-08-28 12:52:00 +0530

    nav-sidebar

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-function log(){
-  document.getElementById("full-card").style.display="block";
-  document.getElementById("body").style.overflow="hidden";
-}
-function logout(){
-  document.getElementById("full-card").style.display="none";
-  document.getElementById("body").style.overflow="auto";
-}
+
 // navbar-background-change
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
@@ -17,3 +10,25 @@ function scrollFunction() {
     console.log("ooppppppppppppppppppppp");
   }
 } 
+//----------------- Login-Logout -------------------
+function log(){
+  document.getElementById("full-card").style.display="block";
+  document.getElementById("body").style.overflow="hidden";
+}
+function logout(){
+  document.getElementById("full-card").style.display="none";
+  document.getElementById("body").style.overflow="auto";
+}
+// -------------------nav-bar---------------------------
+function  openMenu(){
+  console.log("ddddddddddddd")
+  document.getElementById("side-navbar").style.display="block";
+  document.getElementById("nav-opicon").style.display="none"
+  document.getElementById("nav-clicon").style.display="block"
+}
+function  closeMenu(){
+  console.log("ddddddddddddd")
+  document.getElementById("side-navbar").style.display="none";
+  document.getElementById("nav-opicon").style.display="block"
+  document.getElementById("nav-clicon").style.display="none"
+}
