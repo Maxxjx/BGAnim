91ad81eab65cf3b341c89e27c6fb89d85f3950d3
Maxxjx <maxxssj338@example.com>
2022-08-30 20:46:19 +0530

    did

--- script.js
+++ script.js
@@ -4,10 +4,8 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 630 || document.documentElement.scrollTop > 630) {
     document.getElementById("nav-bar").style.backgroundColor="black";
-    console.log("ooooooooooooooooooo");
   } else {
     document.getElementById("nav-bar").style.backgroundColor="transparent";
-    console.log("ooppppppppppppppppppppp");
   }
 } 
 //----------------- Login-Logout -------------------
@@ -20,6 +18,17 @@ function logout(){
   document.getElementById("body").style.overflow="auto";
 }
 // -------------------nav-bar---------------------------
+
+window.screenY=function(){navbar()};
+function navbar(){
+  let x=window.innerWidth
+  document.getElementById("nav-opicon").style.display="block";
+  document.getElementById("nav-clicon").style.display="none";
+if(x>600){
+  document.getElementById("nav-opicon").style.display="none";
+  document.getElementById("nav-clicon").style.display="none";
+}
+}
 function  openMenu(){
   document.getElementById("nav").style.display="block";
   document.getElementById("nav-opicon").style.display="none";
