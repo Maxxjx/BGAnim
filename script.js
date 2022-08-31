cbe1ea08aacdae891984755158ed9ea53ca875e1
Maxxjx <maxxssj338@example.com>
2022-08-31 10:54:57 +0530

    ggggg

--- script.js
+++ script.js
@@ -31,11 +31,13 @@ if(x>600){
 }
 function  openMenu(){
   document.getElementById("nav").style.display="block";
+  document.getElementById("body").style.overflow="hidden";
   document.getElementById("nav-opicon").style.display="none";
   document.getElementById("nav-clicon").style.display="block";
   // document.getElementById("wel-card").style.display="none";
 }
 function  closeMenu(){
+  document.getElementById("body").style.overflow="auto";
   document.getElementById("nav").style.display="none";
   document.getElementById("nav-opicon").style.display="block";
   document.getElementById("nav-clicon").style.display="none";
